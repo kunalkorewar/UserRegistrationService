6b96c974d7efc2846be2b445b3998952c834707c
KUNAL RAJESH KOREWAR <kunal.korewar@gmail.com>
2023-10-13 09:07:49 +0530

    new
    added

--- src/main/java/com/user/registration/api/UserRegistrationServiceApplication.java
+++ src/main/java/com/user/registration/api/UserRegistrationServiceApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserRegistrationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserRegistrationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserRegistrationServiceApplication.class, args);
+
+        System.out.println("tomcat start");
+    }
+
 
 }
